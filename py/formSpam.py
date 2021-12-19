1f895011b80e1810a105fbb47ce1de6e05c0d43a
ItsMurf <brandon.murphy@outlook.com>
2021-12-19 04:05:44 -0700

    clean up

--- py/formSpam.py
+++ py/formSpam.py
@@ -6,15 +6,17 @@ print('Scamwich - formSpam')
 print('Spam online forms with your choice of inputs.')
 print()
 
+
 def initial():
     contIn = ''
     while(contIn != 'Y' and contIn != 'y' and contIn != 'N' and contIn != 'n'):
         contIn = input('Continue? Y/N: ')
 
     if contIn == 'Y' or 'y':
-        url = input('Enter Request URL: ') # request URL destination
-        delayCount = int(input('Delay Time: ')) # seconds between each request
-        spamCount = int(input('Spam Count: ')) # number of forms sent (~17 until flagged)
+        url = input('Enter Request URL: ')  # request URL destination
+        delayCount = int(input('Delay Time: '))  # seconds between each request
+        # number of forms sent (~17 until flagged)
+        spamCount = int(input('Spam Count: '))
         data = {
             'EMAIL': input('Enter Form Email: '),
             'LASTNAME': input('Enter Form Lastname (User): '),
@@ -31,12 +33,14 @@ def initial():
     else:
         print('Try again.')
 
-def main(url,delayCount,spamCount,data):
+
+def main(url, delayCount, spamCount, data):
     for i in range(spamCount):
         response = requests.post(url, data=data).text
         print(response)
 
         time.sleep(delayCount)
+
 
 url, delayCount, spamCount, data = initial()
 main(url, delayCount, spamCount, data)
